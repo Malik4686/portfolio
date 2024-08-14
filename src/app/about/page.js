8f5f7b4cd6a7971892e1301250210410b2dc58c1
Malik4686 <malik00255@gmail.com>
2024-08-14 13:41:47 +0500

    first commit

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
-        Hello world
+        About US!
       
       </h1>
      
--- src/app/about/team/page.js
+++ src/app/about/team/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Team() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
-        Hello world
+        TEAM
       
       </h1>
      
